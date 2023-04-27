c0bf750c909f09ea8bf3b46df11a75b08ed7275b
OlexiyVr <olexiyvr@gmail.com>
2023-04-27 23:57:04 +0300

    11

--- src/js/get_images.js
+++ src/js/get_images.js
@@ -17,39 +17,48 @@ const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '35837972-e713b2afc244ad183858051af';
 
 export async function getImages(name, page = 1) {
-    const params = new URLSearchParams({
-        key: API_KEY,
-        q: name,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-        per_page: 40,
-    });
+  const params = new URLSearchParams({
+    key: API_KEY,
+    q: name,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: 40,
+  });
 
-    const response = await axios.get(`${BASE_URL}?${params}&page=${page}`);
-    const datas = response.data;
-    refs.loadmore.hidden = false;
+  const response = await axios.get(`${BASE_URL}?${params}&page=${page}`);
+  const datas = response.data;
+  refs.loadmore.hidden = false;
 
-    if (page === 1 && datas.totalHits !== 0) {
-        Notiflix.Notify.info(`Hooray! We found ${datas.totalHits} images.`);
-        refs.header.classList.add('opac');
-    }
+  // повідомлення про кількість знайдених зображень
+  if (page === 1 && datas.totalHits !== 0) {
+    Notiflix.Notify.info(`Hooray! We found ${datas.totalHits} images.`);
+    refs.header.classList.add('opac');
+  }
 
-    // Додавання карток
-    refs.card.insertAdjacentHTML('beforend', createMarcup(datas.hits));
+  // додавання карток на екран
+  refs.card.insertAdjacentHTML('beforeend', createMarkup(datas.hits));
 
-    // Прибирання "Load more" по закінченню на сервері карток
-    if (refs.card.childNodes.length + 1 > datas.totalHits && datas.totalHits !== 0) {
-        refs.loadmore.hidden = true;
-        refs.header.classList.remove('opac');
-        Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.");
-    }
+  // прибирання кнопки "Load more" по закінченю карток на сервері
+  if (
+    refs.card.childNodes.length + 1 > datas.totalHits &&
+    datas.totalHits !== 0
+  ) {
+    refs.loadmore.hidden = true;
+    refs.header.classList.remove('opac');
+    Notiflix.Notify.warning(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
 
-    // перевірка за пошуком який відсутній на сервері
-    if (!datas.hits.length) {
-        refs.loadmore.hidden = true;
-        refs.header.classList.remove('opac');
-        Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-        return;
-    }
+  // перевірка за пошуком, який відсутній на сервері
+  if (!datas.hits.length) {
+    refs.loadmore.hidden = true;
+    refs.header.classList.remove('opac');
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
+  // return;
 }
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,6 +1,7 @@
 export default {
-    search: document.qwerySelector('#search-form'),
-    loadmore: document.qwerySelector('.load-more'),
-    card: document.qwerySelector('.gallery'),
-    header:document.querySelector('header'),
-}
+  search: document.querySelector('#search-form'),
+  loadmore: document.querySelector('.load-more'),
+  card: document.querySelector('.gallery'),
+  header: document.querySelector('header'),
+  
+};
