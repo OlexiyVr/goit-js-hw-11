9e916d3ff8668810dfce48b9fcbc5ec4fac2039a
OlexiyVr <olexiyvr@gmail.com>
2023-04-30 12:14:57 +0300

    dy

--- src/index.js
+++ src/index.js
@@ -43,3 +43,17 @@ async function onLoad() {
     console.log(error);
   }
 }
+
+const list = document.querySelector('.scroll-container');
+list.scrollTop = 1;
+list.addEventListener('scroll', function(ev) {
+  let items = this.querySelectorAll('.item');
+  if (parseInt(this.scrollTop) == 0) {
+    this.scrollTop = items[items.length - 1].clientHeight;
+    this.prepend(items[items.length - 1]);
+    this.scrollTop = 1;
+  } else if (this.scrollTop > this.scrollHeight - this.clientHeight - 1) {
+    this.append(items[0]);
+  }
+  return false;
+});
