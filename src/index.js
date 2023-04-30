d559ea4c6779ea8e18143c51544c3c8c70bb3130
OlexiyVr <olexiyvr@gmail.com>
2023-04-30 12:04:11 +0300

    444

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-import OnlyScroll from 'only-scrollbar';
-const scroll = new OnlyScroll(document.querySelector('.scroll-container'));
 
 import './css/styles.css';
 import refs from './js/refs';
